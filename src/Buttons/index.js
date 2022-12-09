5eb2d2b2fcfda6016d9f579adb2906e8c0be7f98
Adrian Wasylik <p27wasylik@gmail.com>
2022-12-09 14:26:35 +0100

    add setAllDone interaction

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
+const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
     tasks.length > 0 && (
         <div className="div__buttons">
             <button
@@ -12,6 +12,7 @@ const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
             <button
                 className="div__button"
                 disabled={tasks.every(({ done }) => done)}
+                onClick={setAllDone}
             >
                 Ukończ wszystkie
             </button>

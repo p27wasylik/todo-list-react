8524e3b952db5a40746495b44613c4f2973741f7
Adrian Wasylik <p27wasylik@gmail.com>
2022-12-13 21:34:13 +0100

    unable to whrite nothing

--- src/App.js
+++ src/App.js
@@ -41,14 +41,17 @@ function App() {
   };
 
   const addNewTask = (content) => {
-    setTasks(tasks => [
-      ...tasks,
-      {
-        content,
-        done: false,
-        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
-      }
-    ])
+    if (content.length > 0) {
+      setTasks(tasks => [
+        ...tasks,
+        {
+          content,
+          done: false,
+          id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+        }
+      ])
+    }
+
   };
 
   return (
